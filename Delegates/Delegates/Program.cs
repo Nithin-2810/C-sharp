b4ea6ce5e7cc36d9892364b7127aff673f97981b
Nithin Kumar M <amnithinkumar2810@gmail.com>
2025-05-21 11:00:58 +0530

    Add folder

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practricee
+{
+    public delegate void del();
+    public class green
+    {
+        public void green1()
+        {
+            Console.WriteLine("You have received green");
+
+            Console.ReadKey();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            green g = new green();
+            del d = g.green1;
+            d();
+
+        }
+    }
+}
